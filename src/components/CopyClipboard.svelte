1382c9234d74911f7713ede64619af9e09a3a3fb
mumbler9486 <mumbler9486@gmail.com>
2022-09-23 00:13:04 +0900

    💄Improve tooltip behaviour on clipboardcopy

--- src/components/CopyClipboard.svelte
+++ src/components/CopyClipboard.svelte
@@ -4,29 +4,35 @@
 
 	export let copyData: string = '';
 
-	const tooltipOnTime = 2000;
+	const tooltipOnTime = 1500;
 	let clicked = false;
 
 	$: tooltipOpenClass = clicked ? 'tooltip-open' : '';
 	$: tooltipClass = classNames('tooltip', tooltipOpenClass);
 
 	const copyToClipboard = () => {
+		navigator.clipboard.writeText(copyData);
+
+		if (clicked) return;
+
 		clicked = true;
+	};
+
+	const mouseLeave = () => {
 		setTimeout(() => {
 			clicked = false;
-		}, 2000);
-		navigator.clipboard.writeText(copyData);
+		}, tooltipOnTime);
 	};
 </script>
 
 {#if clicked}
 	<div class={tooltipClass} data-tip="Copied!">
-		<div class="btn cursor-pointer" on:click={copyToClipboard}>
+		<div class="btn cursor-pointer" on:click={copyToClipboard} on:mouseleave={mouseLeave}>
 			<ClipboardCopy />
 		</div>
 	</div>
 {:else}
-	<div class="btn cursor-pointer" on:click={copyToClipboard}>
+	<div class="btn cursor-pointer" on:click={copyToClipboard} on:mouseleave={mouseLeave}>
 		<ClipboardCopy />
 	</div>
 {/if}
