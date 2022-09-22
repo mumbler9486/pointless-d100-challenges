24bf0b7be6e33acc46ad9727ecf0f2179e7833f4
mumbler9486 <mumbler9486@gmail.com>
2022-09-22 23:10:18 +0900

    ✨ Add website footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<footer
+	class="footer fixed footer-position object-right mt-8 p-8 max-w-xs items-center bg-base-200 text-neutral-content rounded-md"
+	style=""
+>
+	<div>
+		<p>PSO2:NGS is ©SEGA</p>
+	</div>
+	<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+		<a
+			href="https://github.com/mumbler9486/pointless-d100-challenges"
+			target="_blank"
+			class="link link-hover"
+		>
+			GitHub
+		</a>
+	</div>
+</footer>
+
+<style scoped>
+	.footer-position {
+		bottom: 0px;
+		right: 0px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import 'src/app.css';
+	import Footer from 'src/components/Footer.svelte';
 </script>
 
 <main class="container flex flex-col">
 	<div class="mt-10">
 		<slot />
 	</div>
+	<Footer />
 </main>
