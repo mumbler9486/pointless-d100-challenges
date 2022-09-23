1a6d89a756445854ad5e5a431eeb8056e4bc100d
mumbler9486 <mumbler9486@gmail.com>
2022-09-23 14:56:44 +0900

    💄Refactor footer to use more tailwind classes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,13 +1,11 @@
 <footer
-	class="footer fixed footer-position text-right p-4 bg-base-200 text-neutral-content rounded-tl-md"
+	class="footer footer-size fixed bottom-0 right-0 text-right p-4 bg-base-200 text-neutral-content rounded-tl-md"
 >
 	<p>PSO2:NGS is ©SEGA</p>
 </footer>
 
 <style scoped>
-	.footer-position {
-		bottom: 0px;
-		right: 0px;
+	.footer-size {
 		max-width: 10rem;
 	}
 </style>
