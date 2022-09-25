9105e90d6d95671fc8967541ac7bdccd63a27165
mumbler9486 <mumbler9486@gmail.com>
2022-09-25 22:55:31 +0900

    📱Make footer less annoying for portrait displays

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,11 +1,11 @@
 <footer
-	class="footer footer-size fixed bottom-0 right-0 text-right p-4 bg-base-200 text-neutral-content rounded-tl-md"
+	class="flex footer footer-size text-right py-2 px-4 sm:mr-12 xl:mr-0 bg-base-200 text-neutral-content xl:fixed bottom-0 right-0 xl:rounded-tl-md md:rounded md:max-w-40 sm:mt-2 ml-auto"
 >
-	<p>PSO2:NGS is ©SEGA</p>
+	<p class="text-xs">PSO2:NGS<br />©SEGA</p>
 </footer>
 
 <style scoped>
 	.footer-size {
-		max-width: 10rem;
+		max-width: 5.5rem;
 	}
 </style>
