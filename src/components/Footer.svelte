887854ab46e6fcb48e91f1839acfe0eb2990a64b
mumbler9486 <mumbler9486@gmail.com>
2022-09-26 23:39:04 +0900

    💄Improve footer layout classes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer
-	class="flex footer footer-size text-right py-2 px-4 sm:mr-12 xl:mr-0 bg-base-200 text-neutral-content xl:fixed bottom-0 right-0 xl:rounded-tl-md md:rounded md:max-w-40 sm:mt-2 ml-auto"
+	class="flex footer footer-size text-right py-2 px-4 mr-12 mt-2 bottom-0 right-0 bg-base-200 text-neutral-content rounded ml-auto xl:fixed xl:mr-0 xl:rounded-none xl:rounded-tl-md"
 >
 	<p class="text-xs">PSO2:NGS<br />©SEGA</p>
 </footer>
