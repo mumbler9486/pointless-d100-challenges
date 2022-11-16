8d20efb101791ab6895282e097c4199262fb6de6
mumbler9486 <87349460+mumbler9486@users.noreply.github.com>
2022-11-16 22:46:39 +0900

    🔥 Teardown github pages and add redirect page

--- _app/immutable/chunks/0-292b2665.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-c3e9d4a3.js";export{t as component};
--- _app/immutable/chunks/2-e3f2aa55.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-bb169d4e.js";export{t as component};
